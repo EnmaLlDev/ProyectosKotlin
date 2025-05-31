4083a3ef5190797b5b5824b811a4c5ea44223635
a24moiseslp <a24moiseslp@iessanclemente.net>
2025-05-31 16:24:32 +0200

    Main.kt

--- Main.kt
+++ Main.kt
@@ -1,3 +1,11 @@
 fun main(){
-
+    var caso= readlnOrNull()
+    while (caso != null){
+       var (planta, habitacion, pRata, hRata)= caso.split(" ").map { it.toInt() }
+       var casa = Array(planta){CharArray(habitacion)}
+        caso= readlnOrNull()
+    }
 }
+
+
+
