8e8545b41e6bf61dc671f71a02921f947060b85e
a24moiseslp <a24moiseslp@iessanclemente.net>
2025-05-31 16:31:12 +0200

    Main.kt modificado

--- Main.kt
+++ Main.kt
@@ -1,10 +1,25 @@
 fun main(){
     var caso= readlnOrNull()
     while (caso != null){
-       var (planta, habitacion, pRata, hRata)= caso.split(" ").map { it.toInt() }
-       var casa = Array(planta){CharArray(habitacion)}
+        var (planta, habitacion, pRata, hRata)= caso.split(" ").map { it.toInt() }
+        var casa = Array(planta){CharArray(habitacion)}
+        crearCasa(casa)
+        rodearRata(casa, pRata - 1, hRata - 1)
+        imprimirCasa(casa)
         caso= readlnOrNull()
     }
+}
+
+fun imprimirCasa(casa: Array<CharArray>) {
+    TODO("Not yet implemented")
+}
+
+fun rodearRata(casa: Array<CharArray>, pRata: Int, hRata: Int) {
+
+}
+
+fun crearCasa(casa: Array<CharArray>) {
+    TODO("Not yet implemented")
 }
 
 
