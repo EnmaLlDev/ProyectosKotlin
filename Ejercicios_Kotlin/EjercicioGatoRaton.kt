37374f983d48fe11fa8606086d5129691cdc8e20
a24moiseslp <a24moiseslp@iessanclemente.net>
2025-06-04 11:11:40 +0200

    Cambios en EjercicioGatoRaton

--- Ejercicios_Kotlin/EjercicioGatoRaton.kt
+++ Ejercicios_Kotlin/EjercicioGatoRaton.kt
@@ -47,46 +47,43 @@ fun rodearRata(casa: Array<CharArray>, pRata: Int, hRata: Int) {
 }
  */
 fun main(){
-    var caso= readlnOrNull()
-    while (caso != null){
-        val (planta, habitacion, pRata, hRata)= caso.split(" ").map { it.toInt() }
-        val casa= Array(planta){CharArray(habitacion)}
-        iniciarCasa(casa)
-        rodearRata(casa, pRata -1, hRata -1)
-        dibujarCasa(casa)
-        caso= readlnOrNull()
-    }
+    generateSequence { readlnOrNull() }
+        .takeWhile { it != null }
+        .forEach { caso ->
+            val (planta, habitaciones, pRata, hRata)=caso.split(" ").map { it.toInt() }
+            val casa= crearCasa(planta, habitaciones)
+            val casaConRataYGatos= rodearRata(casa, pRata-1, hRata-1)
+            imprimirCasa(casaConRataYGatos)
+            println()
+        }
 }
 
-fun dibujarCasa(casa: Array<CharArray>) {
-    for (planta in casa.indices){
-        for (hab in casa[planta].indices){
-            print("[${casa[planta][hab]}]")
+fun imprimirCasa(casa: Array<CharArray>) {
+    for (planta in casa){
+        for (hab in planta){
+            print("[${hab}]")
         }
         println()
     }
-    println()
 }
 
-fun rodearRata(casa: Array<CharArray>, pRata: Int, hRata: Int) {
-    casa[pRata][hRata]='R'
-    for (planta in pRata -1..pRata+1){
-        for (hab in hRata -1..hRata+1){
-            if (planta>=0 && planta<casa.size && hab>=0 && hab<casa[planta].size){
-                if (casa[planta][hab] != 'R'){
-                    casa[planta][hab]='G'
-                }
+fun rodearRata(casa: Array<CharArray>, pRata: Int, hRata: Int):Array<CharArray> {
+    val nuevaCasa=casa.map { it.copyOf() }.toTypedArray()
+    nuevaCasa[pRata][hRata]='R'
+    for (planta in -1..1){
+        for (hab in -1..1){
+            val nuevaPlanta=pRata+planta
+            val nuevaHab=hRata+hab
+            if (nuevaPlanta in nuevaCasa.indices && nuevaHab in nuevaCasa[0].indices && (planta != 0 || hab != 0)){
+                if (nuevaCasa[nuevaPlanta][nuevaHab] != 'R') nuevaCasa[nuevaPlanta][nuevaHab] = 'G'
             }
         }
     }
+    return nuevaCasa
 }
 
-fun iniciarCasa(casa: Array<CharArray>) {
-    for (planta in casa.indices){
-        for (hab in casa[planta].indices){
-            casa[planta][hab]='X'
-        }
-    }
-}
+fun crearCasa(planta: Int, habitaciones: Int): Array<CharArray> =Array(planta){ CharArray(habitaciones) {'X'} }
+
+
 
 
